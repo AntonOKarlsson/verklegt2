1b9b8af425e9e9cd7101ad7c1113d9ca6b317d60
arnthorsig <addisig@outlook.com>
2025-05-06 20:42:23 +0000

    Update views.py

--- castle_apartments/property/views.py
+++ castle_apartments/property/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from  property.models import Property
+
 
 def index(request):
-    #return render(request, )
+    #return render(request, 'test.html',{"properties:" Property.objects.all()} )
     return HttpResponse('Response from property')
 
