766a4ed94de6b0189e44206043051d8c8ac31799
Anton Örn Karlsson <anton.orn.karlsson@gmail.com>
2025-05-07 20:38:05 +0000

    vinstri valstika horfin, allir valkostir komnir í valstiku efst

--- castle_apartments/properties/urls.py
+++ castle_apartments/properties/urls.py
@@ -4,5 +4,5 @@ from .views import property_search
 
 urlpatterns = [
     path('<int:id>/', views.get_property_by_id, name='property-by-id'),
-    path('search', property_search, name='property-search'),
+    path('search', property_search, name='properties/search'),
 ]
