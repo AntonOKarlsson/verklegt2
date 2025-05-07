5285235723fcfb31b5d735175b7f94a652156361
arnthorsig <addisig@outlook.com>
2025-05-07 13:54:27 +0000

    prófanir með logo

--- castle_apartments/castle_apartments/urls.py
+++ castle_apartments/castle_apartments/urls.py
@@ -21,7 +21,8 @@ from django.urls import path, include
 from django.contrib import admin
 from django.urls import path, include
 from .views import test_view, home, property_view
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
