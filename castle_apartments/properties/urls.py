e709f7126097cd1576a2a59b9c250a4fbb23cb22
brynjarv24 <brynjarv24@ru.is>
2025-05-13 23:00:00 +0000

    Update urls.py
    urls for edit and thumbnails

--- castle_apartments/properties/urls.py
+++ castle_apartments/properties/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('<int:id>/', views.get_property_by_id, name='property-by-id'),
     path('<int:id>/offer/', views.offer_on_property_by_id, name='offer-by-id'),
     path('add_property/', views.add_property2, name='property-add'),
+    path('edit/<int:property_id>/', views.edit_property, name='edit_property'),
+    path('set-thumbnail/<int:image_id>/', views.set_thumbnail, name='set_thumbnail'),
     ]
