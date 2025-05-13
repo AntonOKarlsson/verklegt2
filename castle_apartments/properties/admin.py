d62c3ca58e5ca99253634c71a03e6b040c46fb2a
arnthorsig <addisig@outlook.com>
2025-05-13 16:15:17 +0000

    update admin.py

--- castle_apartments/properties/admin.py
+++ castle_apartments/properties/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Property
 
 # Register your models here.
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ('title', 'seller', 'price', 'postal_code', 'property_type')
+    search_fields = ('title', 'address', 'postal_code')
+    list_filter = ('property_type', 'built_year', 'seller')
