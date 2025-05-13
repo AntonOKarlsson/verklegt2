fcddc18f6877dcc7cc098058579832209eecd666
arnthorsig <addisig@outlook.com>
2025-05-13 18:09:00 +0000

    tók út about

--- castle_apartments/castle_apartments/urls.py
+++ castle_apartments/castle_apartments/urls.py
@@ -27,7 +27,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('properties/', include('properties.urls')),
     path('', home, name='home'),
-    path('about/', name='about'),
     path('user/', include('user.urls')),
 ]
 
--- castle_apartments/castle_apartments/views.py
+++ castle_apartments/castle_apartments/views.py
@@ -5,6 +5,3 @@ from properties.models import Property
 
 def home(request):
     return render(request,'home.html')
-
-def about(request):
-    return render(request,'about.html')
